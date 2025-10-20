3510d489529af7677ecc0a6595d58186d49bfef7
clemensjoe11-rgb <clemensjoe11@gmail.com>
2025-10-20 14:37:55 +0200

    Delete api directory

--- api/me.cjs
+++
@@ -1,7 +0,0 @@
-// api/me.js
-exports.config = { runtime: "nodejs" };
-
-module.exports = async function handler(_req, res) {
-  // Placeholder. Implement Cookie/Token-Check hier.
-  res.status(200).json(null);
-};
